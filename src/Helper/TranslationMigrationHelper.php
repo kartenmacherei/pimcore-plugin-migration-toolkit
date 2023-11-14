de04d420ab90cfa2e0d9c409fc596b50e36a8879
kerstin.gerull <kerstin.gerull@basilicom.de>
2023-11-14 10:38:27 +0100

    feat(): do not catch exception

--- src/Helper/TranslationMigrationHelper.php
+++ src/Helper/TranslationMigrationHelper.php
@@ -7,35 +7,33 @@ use Pimcore\Model\Translation;
 
 class TranslationMigrationHelper extends AbstractMigrationHelper
 {
+    /**
+     * @throws Exception
+     */
     public function addTranslations(array $translations, string $domain = 'messages'): void
     {
         foreach ($translations as $key => $languages) {
-            try {
-                $trans = Translation::getByKey($key, $domain);
-                if (empty($trans)) {
-                    $trans = new Translation();
-                    $trans->setKey($key);
-                    $trans->setDomain($domain);
-                }
-                foreach ($languages as $language => $translation) {
-                    $trans->addTranslation($language, $translation);
-                }
-                $trans->save();
-            } catch (Exception) {
-                // nothing to do
+            $trans = Translation::getByKey($key, $domain);
+            if (empty($trans)) {
+                $trans = new Translation();
+                $trans->setKey($key);
+                $trans->setDomain($domain);
             }
+            foreach ($languages as $language => $translation) {
+                $trans->addTranslation($language, $translation);
+            }
+            $trans->save();
         }
     }
 
+    /**
+     * @throws Exception
+     */
     public function removeTranslationsByKey(array $keys, string $domain = 'messages'): void
     {
         foreach ($keys as $key) {
-            try {
-                $trans = Translation::getByKey($key, $domain);
-                $trans?->delete();
-            } catch (Exception) {
-                // nothing to do
-            }
+            $trans = Translation::getByKey($key, $domain);
+            $trans?->delete();
         }
     }
 }
